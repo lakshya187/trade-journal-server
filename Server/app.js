a19722f1a11bb187ca0ab203afd91271ba715f9f
Lakshya Tiwari <tlakshya187@gmail.com>
2022-04-01 14:14:33 +0530

    API docs

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const tradeRouter = require(`${__dirname}/routes/tradeRoutes`);
+const swaggerJsDocs = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 app.use(express.json());
-
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Trade Journal",
+      description: "Journal your trades",
+      servers: ["http://localhost:5000"],
+    },
+  },
+  apis: ["./routes/tradeRoutes.js"],
+};
+const swaggerDocs = swaggerJsDocs(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/trades", tradeRouter);
-
 module.exports = app;
--- Server/routes/tradeRoutes.js
+++ Server/routes/tradeRoutes.js
@@ -1,6 +1,67 @@
 const express = require("express");
 const tradeControllers = require("./../controllers/tradeControllers");
 
+/**
+ * @swagger
+ * /:2
+ *  get:
+ *    summary : This endpoint is to get all the trades.
+ *    description : Use to request all the trades.
+ *    responses :
+ *        200:
+ *            description : To test the get method
+ *
+ */
+
+/**
+ * @swagger
+ * /:
+ *  post:
+ *    summary : This endpoint is to create new trades.
+ *    description : expecting data in the format.
+ *    parameters: [stockTicker : string]
+ *    responses :
+ *        201:
+ *            description : To test the post method
+ *
+ */
+/**
+ * @swagger
+ * /id:
+ *  patch:
+ *    summary : This endpoint is to update trades.
+ *    description : expecting data in the format.
+ *
+ *    responses :
+ *        202:
+ *            description : To test the patch method
+ *
+ */
+
+/**
+ * @swagger
+ * /id:
+ *  delete:
+ *    summary : This endpoint is to delete trades.
+ *    description : enter add the trade id.
+ *
+ *    responses :
+ *        203:
+ *            description : To test the delete method
+ *
+ */
+/**--++
+ * @swagger
+ * /id:
+ *  get:
+ *    summary : This endpoint is get a single trade.
+ *    description : expecting data in the format.
+ *
+ *    responses :
+ *        200:
+ *            description : To test the get method
+ *
+ */
 const router = express.Router();
 router
   .route("/")
